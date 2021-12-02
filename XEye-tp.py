73c8ebadf0b20a0ea49f0ae4e99f5ed54590476b
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2021-12-02 13:21:58 +0200

    Update XEye-tp.py

--- XEye-tp.py
+++ XEye-tp.py
@@ -14,10 +14,10 @@ def Checkexis():
         print("\n [Info] --> Wifi USB adapter is already set to Monitor mode, Exiting ...... ")
         exit()
     elif chwlann:
-       asking = input("\n [Permission] --> Would you like to set your Wifi USB adapter to Monitor now or start over?  [Monitor / Start over] ")
-       if asking.lower() == 'monitor':
+       asking = input("\n [Permission] --> Would you like to set your Wifi USB adapter to \"Monitor mode now\" or \"reconfigure\"?  [set / reconf] ")
+       if asking.lower() == 'set':
             tp_set()
-        elif asking.lower() == "start over":
+        elif asking.lower() == "reconf":
            tp_conf()
         else:
             print(" [Warning] --> Invalid Entry. [Your interface is just set to Auto mode]   Exiting .....")
