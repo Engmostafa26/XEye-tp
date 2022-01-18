f602e0ddc58cc76e39ad40f674106cec2961cd31
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2022-01-18 18:55:12 +0200

    Update XEye-tp.py

--- XEye-tp.py
+++ XEye-tp.py
@@ -7,9 +7,9 @@ def Checkroot():
     who = subprocess.check_output('whoami')
     chuser = re.search(r"root", str(who))
     if chuser:
-        verifywus = input("\n [Verifying] --> The tool needs to run with root user not only running with \"sudo\". Are your running your shell as root? [yes / no] " )
+        verifywus = input("\n [Verifying] --> The XEye-tp tool needs to run with root user not only running with \"sudo\". Are your running your shell as root? [yes / no] " )
         if verifywus.lower() == 'y' or verifywus.lower() == 'yes':
-            Checkexis()
+            Start()
         elif verifywus.lower() == 'n' or verifywus.lower() == 'no':
             print(" [Instruction] --> Please run \"sudo su\" command then enter the password for the current user to change to root with the present directory, then run the tool again :) \n")
         else:
@@ -17,127 +17,256 @@ def Checkroot():
             exit()
     else:
         print("\n\n [Warning] --> You are not root - Please read and follow the instructions below: \n ")
-        print("\n [Instruction] --> 1- Please run the tool with root user not only with \"sudo\" to configure your adapter with no issues ")
+        print("\n [Instruction] --> 1- Please run the XEye-tp tool with root user not only with \"sudo\" to configure your adapter with no issues. ")
         print(" [Instruction] --> 2- Run \"sudo su\" command then enter the password for the current user to change to root with the present directory, then run the tool again :) \n")
-def Checkexis():
-    print("\n [*] --> Welcome to XEye-tp tool to set your \"tp-link\" model \"TL-WN722N\" Wifi USB adapter to Monitor mode :):):) \n\n")
+def Start():
+    print("\n ************************************************************************************************************************************************************")
+    print("\n [Welcoming] --> Welcome to XEye-tp tool :):):) \n\n")
+    print(" [Info] --> With XEye-tp tool, you can do the following: \n")
+    print(" \t[*] --> 1) Change your Wifi USB adapter TP-Link model WN722N version 2 and 3 to Monitor mode easily and in few minutes or even seconds :) ")
+    print(" \t[*] --> 2) You can easily change the Mac address after your TP-Link Wifi USB is set to monitor mode :) \n")
     ifconfig_outp = subprocess.check_output('iwconfig')
     chwlan = re.search(r"WIFI@REALTEK", str(ifconfig_outp))
     chwlann = re.search(r"Mode:Auto", str(ifconfig_outp))
     chwlannn = re.search(r"Mode:Monitor", str(ifconfig_outp))
     if chwlannn:
-        print("\n [Info] --> Your Wifi USB adapter is already set to Monitor mode, Exiting ...... ")
-        print("\n [Recommendation] --> Run the \"exit\" command to exit the root shell, and stay secure :) ")
-        exit()
-    elif chwlann:
-       print("\n [Info] --> Your Wifi USB adapter is already set to Auto mode ")
-       asking = input(" [Permission] --> Would you like to set your Wifi USB adapter to \"Monitor mode now\" or \"reconfigure\"?  [set / reconf] ")
-       if asking.lower() == 'set':
-            tp_set()
-       elif asking.lower() == 'reconf':
-            tp_conf()
-       else:
-            print(" [Warning] --> Invalid Entry. [Your interface is just set to Auto mode]   Exiting .....")
-            print("\n [Recommendation] --> Run the \"exit\" command to exit the root shell, and stay secure ;) ")
+        lines()
+        print("\n [Info] --> Your Wifi USB adapter is already set to Monitor mode ")
+        Asking = input("\n [Asking] --> Would like to change your adapter Mac address? [yes / no] ")
+        lines()
+        if Asking.lower() == 'y' or Asking.lower() == 'yes':
+            Usermd()
+        elif Asking.lower() == 'n' or Asking.lower() == 'no':
+            lines()
+            print("\n [*] --> Thanks for using XEye-tp tool, Your Adapter is just set to Monitor mode - Bye bye .....\n")
+            lines()
+        else:
+            lines()
+            print(" [warning] --> Invalid entry, please use a yes or no answer, Exiting .....")
+            lines()
             exit()
+    elif chwlann:
+        lines()
+        print("\n [Info] --> Your Wifi USB adapter is already set to Auto mode ")
+        asking = input(" [Permission] --> Would you like to set your Wifi USB adapter to \"Monitor mode now\" or \"reconfigure\"?  [set / reconf] ")
+        lines()
+        if asking.lower() == 'set':
+           tp_set()
+        elif asking.lower() == 'reconf':
+           tp_conf()
+        else:
+           lines()
+           print(" [Warning] --> Invalid Entry. [Your interface is just set to Auto mode]   Exiting .....")
+           lines()
+           exit()
     elif chwlan:
         usermd()
     else:
-        print("\n [Warning] --> Please make sure that the Wifi USB is plugged in, Exiting ...... ")
+        lines()
+        print("\n [Warning] --> Please make sure that the Wifi USB is plugged in, Bye bye ...... ")
+        lines()
         exit()
-
+    TheEnd()
 def usermd():
+    lines()
     sque= input("\n [Permission] --> Would you like to proceed? [yes / no] ")
+    lines()
     if sque.lower() == 'y' or sque.lower() == 'yes':
         tp_conf()
     elif sque.lower() == 'n' or sque.lower() == 'no':
+        lines()
         print("\n [*] --> Thanks for using XEye-tp tool, Exiting .....\n")
-        print("\n [Recommendation] --> Run the \"exit\" command to exit the root shell, and stay secure ;) \n")
-        print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert")
-        
-        exit()
+        lines()
     else:
+        lines()
         print(" [warning] --> Invalid entry, please use a yes or no answer, Exiting .....")
+        lines()
         exit()
 
 def tp_conf():
- 
+    lines()
     print("\n [*] --> Updating your system, please wait ....  \n")
+    lines()
     subprocess.call(['apt', 'update', '-y'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Installing build-essentials, Please wait ....  \n")
+    lines()
     subprocess.call(['apt', 'install', 'build-essential', '-y'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Installing bc, Won't take too long :) ....  \n")
+    lines()
     subprocess.call(['apt', 'install', 'bc', '-y'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Installing libelf-dev, Please wait ....  \n")
+    lines()
     subprocess.call(['apt', 'install', 'libelf-dev', '-y'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Installing the required linux-headers, Please wait .....  \n")
+    lines()
     subprocess.call("apt install linux-headers-$(uname -r)", shell=True)
+    lines()
     print("\n [*] --> Removing \"r8188eu.ko module\"  \n")
+    lines()
     subprocess.call(['rmmod', 'r8188eu.ko'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Git cloning \"rtl8188eus\"  \n")
+    lines()
     unamer = subprocess.check_output(['uname','-r'])
     unamerr = re.search(r"5.15", str(unamer))
     if unamerr.group(0):
         subprocess.call(['git', 'clone', 'https://github.com/drygdryg/rtl8188eus.git'], stdout=subprocess.DEVNULL)
     else:
         subprocess.call(['git', 'clone', 'https://github.com/aircrack-ng/rtl8188eus'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Installing dkms  \n")
+    lines()
     subprocess.call(['apt', 'install', 'dkms'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Done installing dkms. proceeding further ....  \n")
+    lines()
     os.chdir("rtl8188eus")
+    lines()
     print("\n [*] --> Echoing \"blacklist r8188eu.ko\" to \"realtek.conf\"  \n")
+    lines()
     subprocess.call("echo \"blacklist r8188eu.ko\" > \"/etc/modprobe.d/realtek.conf\"", shell=True)
+    lines()
     print("\n [*] --> Running Make command, Will take few minutes, please wait and ignore the upcoming errors and warnings ......  \n")
+    lines()
     subprocess.call(['sudo', 'make'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Running Make Install command  \n")
+    lines()
     subprocess.call(['sudo', 'make', 'install'], stdout=subprocess.DEVNULL)
+    lines()
     print("\n [*] --> Running \"modprobe 8188eu\"  \n")
+    lines()
     subprocess.call("sudo modprobe 8188eu", shell=True)
     iwco = subprocess.check_output(['iwconfig'])
     Auto_check = re.search(r"Mode:Auto", str(iwco))
     if not Auto_check:
+        lines()
         print("\n [Warning] --> The Wifi adapter mode is not Auto or it is just missing. ")
-        print(" [Instruction] --> UnPlug and plug in your Wifi USB adapter, wait for few seconds then run the tool again with root :)  \n")
+        print(" [Instruction] --> UnPlug and plug in your Wifi USB adapter, wait for few seconds then run the tool again with root - Bye bye :)   \n")
+        lines()
         exit()
     if Auto_check.group(0) == 'Mode:Auto':
+        lines()
         print("\n [Congrats] --> The Wifi USB adapter is successfully configured \n")
         asking = input("\n [Permission] --> Would you like to set your Wifi USB adapter to Monitor mode now?  [yes / no] ")
+        lines()
         if asking.lower() == 'y' or asking.lower() == 'yes':
             tp_set()
         elif asking.lower() == 'n' or asking.lower() == 'no':
+            lines()
             print("\n [Info] --> Now your adapter is just set to Auto mode - Bye Bye :) \n")
-            print("\n [Recommendation] --> Run the \"exit\" command to exit the root shell, and stay secure ;) ")
-            print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert")
-            subprocess.call("exit",shell=True)
-            exit()
+            lines()
         else:
-            print("\n [Warning] --> Invalid Entry. [Your interface is just set to Auto mode]   Exiting .....\n")
+            lines()
+            print("\n [Warning] --> Invalid Entry. [Your interface is just set to Auto mode] - Exiting .....\n")
+            lines()
             exit()
 def tp_set():
-        interfs = subprocess.check_output('iwconfig')
-        interf = re.search(r"\w\w\w\w\d", str(interfs))
-        interff = re.search(r"\w\w\w\d", str(interfs))
-        Interff = re.search(r"WIFI@REALTEK", str(interfs))
-        if interf:
-            interf = interf.group(0)
-        elif interff and Interff:
-            interf = interff.group(0)
-        subprocess.call(['sudo', 'ifconfig', interf, 'down'])
-        subprocess.call("sudo airmon-ng check kill", shell=True)
-        subprocess.call(['sudo', 'iwconfig', interf, 'mode', 'monitor'])
-        subprocess.call(['sudo', 'ifconfig', interf, 'up'])
-        tp_check()
+    interf = getinterf()
+    subprocess.call(['ifconfig', interf, 'down'])
+    subprocess.call("airmon-ng check kill", shell=True)
+    subprocess.call(['iwconfig', interf, 'mode', 'monitor'])
+    subprocess.call(['ifconfig', interf, 'up'])
+    tp_check()
 def tp_check():
     iwcon = subprocess.check_output(['iwconfig'])
     iwcon_Mcheck = re.search(r"Monitor",str(iwcon))
     if iwcon_Mcheck.group(0) == "Monitor":
-        print("\n [Congrats] --> You wireless USB has been set to monitor mode, Bye Bye :) :) \n")
-        print("\n [Recommendation] --> 1- Run the \"exit\" command to exit the root shell, and stay secure ;) ")
-        print(" [Recommendation] --> 2- If you want to change the MAC address, clone XEye-Machanger tool from this link: \" https://github.com/Engmostafa26/XEye-MacChanger-v3.2\" ")
-        print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert")
-        exit()
+        lines()
+        print("\n [Congrats] --> You wireless USB has been set to monitor mode :) :) \n")
+        Asking = input("\n [Asking] --> Would like to change your adapter Mac address? [yes / no] ")
+        lines()
+        if Asking.lower() == 'y' or Asking.lower() == 'yes':
+            Usermd()
+        elif Asking.lower() == 'n' or Asking.lower() == 'no':
+            lines()
+            print("\n [*] --> Thanks for using XEye-tp tool, Your Adapter is just set to Monitor mode - Bye bye :) .....\n")
+            lines()
+        else:
+            lines()
+            print(" [warning] --> Invalid entry, please use a yes or no answer, Exiting .....")
+            lines()
+            exit()
     else:
-        print("\n [Failed] --> Sorry :( , your Wifi USB could not be changed to monitor mode ")
-        print("\n [Instruction] --> Upgrade and Restart your system then try again ")
+        lines()
+        print("\n [Failure] --> Sorry :( , your Wifi USB could not be changed to monitor mode ")
+        print("\n [Instruction] --> Upgrade and Restart your system then run the tool again ")
+        lines()
         exit()
+def Usermd():
+    Interface = getinterf()
+    lines()
+    Mac = input("\n         --> Enter the required Mac address: ")
+    if not Mac:
+        print(" \n [Warning] --> No Mac address specified. ")
+        print(" \n [Info] Now your adapter is just set to monitor mode - Exiting .....")
+        lines()
+        exit()
+    Macc1 = getmac(Interface)
+    if Macc1 == Mac:
+        print(" \n [Info] --> You just entered the same Mac address for " + Interface + ", please enter a different Mac address - Exiting ...... ")
+        lines()
+        exit()
+    ChMac(Interface, Mac)
+    Macc2 = getmac(Interface)
+    if (Macc2 == Mac) or (Macc1 != Macc2):
+        print(" [Done] --> The Mac address is changed successfully to " + Mac)
+        lines()
+    else:
+        print(" [Warning] --> Something Went wrong, The Mac address didn't change to "+Mac)
+        print("\n [Instruction] --> Make sure that you entered a valid Mac address, or unplug then plug in you adapter, wait for few seconds then run the tool again - Bye bye :) ..... ")
+        lines()
+        exit()
+
+def ChMac(Interface,Mac):
+    lines()
+    print("\n [Info] --> XEye-tp is setting your " + Interface + " Mac address to " + Mac)
+    lines()
+    subprocess.call(["ifconfig", Interface, "down"])
+    subprocess.call(["iwconfig", Interface, "mode", "Auto"])
+    subprocess.call(["ifconfig", Interface, "hw", "ether", Mac])
+    subprocess.call(["iwconfig", Interface, "mode", "Monitor"])
+    subprocess.call(["ifconfig", Interface, "up"])
+
+def getinterf():
+    interfs = subprocess.check_output('iwconfig')
+    interf = re.search(r"\w\w\w\w\d", str(interfs))
+    interff = re.search(r"\w\w\w\d", str(interfs))
+    Interff = re.search(r"WIFI@REALTEK", str(interfs))
+    if interf and Interff:
+        return interf.group(0)
+    elif interff and Interff:
+        return interff.group(0)
+    else:
+        lines()
+        print(" [Warning] --> Couldn't read your adapter, please make sure that your adapter is plugged in or simply replug it then run the tool again - Exiting .......")
+        lines()
+        exit()
+def getmac(interface):
+    ifconfgi_re = subprocess.check_output(["ifconfig", interface])
+    testing1 = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfgi_re))
+    testing2 = re.search(r"\w\w-\w\w-\w\w-\w\w-\w\w-\w\w", str(ifconfgi_re))
+    if testing1:
+        return testing1.group(0)
+    elif testing2:
+        mvalue = testing2.group(0)
+        Mvalue = re.sub('-',':',mvalue)
+        return Mvalue
+    else:
+        lines()
+        print("\n [Warning] --> Please make sure that your adapter is plugged in, then run the tool again and use the set option. - Exiting ..... ")
+        lines()
+        exit()
+def lines():
+    print("-------------------------------------------------------------------------------------------------------")
+def TheEnd():
+    lines()
+    print("\n [Recommendation] --> Run the \"exit\" command to exit the root shell, and stay secure :) \n")
+    lines()
+    print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert and \"XEye\" founder")
+    exit()
 Checkroot()
