1e3f57288d341b37dbf1d072ed207648312d22c8
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2022-08-10 01:02:27 +0200

    Update XEye-tp.py

--- XEye-tp.py
+++ XEye-tp.py
@@ -5,7 +5,7 @@ import re
 import time
 import scapy.all as sc
 
-print("\n\n\n\n\n\t\t\t[Attention] --> The XEye-tp tool is being updated at the moment and it might not work properly, if the tool exit or showed error, please try again in few minutes\n\n\n\n\n"
+print("\n\n\n\n\n\t\t\t[Attention] --> The XEye-tp tool is being updated at the moment and it might not work properly, if the tool exit or showed error, please try again in few minutes\n\n\n\n\n")
 def udte():
     print("\n[Info] --> The XEye-tp tool will check for its updates, please wait .....\n\n")
     time.sleep(3)
