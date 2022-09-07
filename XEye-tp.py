c7cb778be08060358c159d83f21fa199bcd340b2
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-09-07 16:35:36 +0200

    Update XEye-tp.py

--- XEye-tp.py
+++ XEye-tp.py
@@ -87,7 +87,7 @@ def Start():
             Usermd()
         elif Asking.lower() == 'n' or Asking.lower() == 'no':
             lines()
-            defau = input("\n [Reset] --> If you want to reset your adapter, you just need to reboot your OS - Reboot now? [yes/no]")
+            defau = input("\n [Reset] --> If you want to reset your adapter, you just need to reboot your OS - Reboot now? [yes/no] ")
             if defau.lower() == 'yes' or defau.lower() == 'y':
                 print("\n [Attention] --> We are rebooting your OS in 20 seconds, you can cancel by pressing on the left \"ctrl+c\" ")
                 time.sleep(20)
