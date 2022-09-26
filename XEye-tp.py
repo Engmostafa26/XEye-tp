b2d7aad0f98fc83850b17db50667bbdb8c4c31b6
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-09-26 17:51:34 +0200

    Update XEye-tp.py

--- XEye-tp.py
+++ XEye-tp.py
@@ -395,6 +395,7 @@ def printr(ipss):
     time.sleep(2)
     askii = input("\n\n [Asking] --> Would you like to start ARP spoofing attack? ")
     if askii.lower() == 'y' or askii.lower() == 'yes':
+        subprocess.call("echo 1 > /proc/sys/net/ipv4/ip_forward",shell=True)
         arpspoof()
     elif askii.lower() == 'n' or askii.lower() == 'no':
         time.sleep(1)
